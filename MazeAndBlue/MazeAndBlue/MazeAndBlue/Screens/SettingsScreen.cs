d028dbc7a8fdf0df967fc48c84e00c597c7d40b1
Teja Ravipati <ravipati@umich.edu>
2013-03-28 13:57:19 -0400

    settings screen designed

--- MazeAndBlue/MazeAndBlue/MazeAndBlue/Screens/SettingsScreen.cs
+++ MazeAndBlue/MazeAndBlue/MazeAndBlue/Screens/SettingsScreen.cs
@@ -6,19 +6,37 @@ namespace MazeAndBlue
 {
     public class SettingsScreen
     {
-        Button backButton, plRHand, plLHand, p2RHand, p2LHand, roomQuiet, roomAver, roomLoud;
+        Texture2D texture;
+        Button backButton, plRHand, plLHand, p2RHand, p2LHand, roomQuiet, roomAver, roomLoud, 
+                soundsOn, soundsOff, setBackground, setGoalImage;
         List<Button> buttons;
+        Rectangle screenRectangle;
 
         public SettingsScreen()
         {
-            backButton = new Button(new Point(150, 0), 100, 40, "", "Buttons/back");
-            plRHand = new Button(new Point(0, 60), 100, 40, "plRHand", "Buttons/button");
-            plLHand = new Button(new Point(150, 60), 100, 40, "plLHand", "Buttons/button");
-            p2RHand = new Button(new Point(0, 120), 100, 40, "p2RHand", "Buttons/button");
-            p2LHand = new Button(new Point(150, 120), 100, 40, "p2LHand", "Buttons/button");
-            roomQuiet = new Button(new Point(0, 180), 100, 40, "roomQuiet", "Buttons/button");
-            roomAver = new Button(new Point(150, 180), 100, 40, "roomAver", "Buttons/button");
-            roomLoud = new Button(new Point(300, 180), 100, 40, "roomLoud", "Buttons/button");
+            int screenWidth = Program.game.screenWidth;
+            int screenHeight = Program.game.screenHeight;
+            screenRectangle = new Rectangle(0, 0, screenWidth, screenHeight);
+
+            int smallButtonWidth = 136;
+            int smallButtonHeight = 72;
+            int largeButtonWidth = 170;
+            int largeButtonHeight = 92;
+
+            backButton = new Button(new Point(screenWidth / 8, 50), smallButtonWidth, smallButtonHeight, "", "Buttons/back");
+            plLHand = new Button(new Point(screenWidth / 2 + smallButtonWidth, 170), smallButtonWidth, smallButtonHeight, "plLHand", "Buttons/left");
+            plRHand = new Button(new Point(screenWidth / 2 + 3 * smallButtonWidth, 170), smallButtonWidth, smallButtonHeight, "plRHand", "Buttons/right");
+            p2LHand = new Button(new Point(screenWidth / 2 + smallButtonWidth, 275), smallButtonWidth, smallButtonHeight, "p2LHand", "Buttons/left");
+            p2RHand = new Button(new Point(screenWidth / 2 + 3 * smallButtonWidth, 275), smallButtonWidth, smallButtonHeight, "p2RHand", "Buttons/right");
+            roomQuiet = new Button(new Point(screenWidth / 2 - smallButtonWidth, 380), smallButtonWidth, smallButtonHeight, "roomQuiet", "Buttons/low");
+            roomAver = new Button(new Point(screenWidth / 2 + smallButtonWidth, 380), smallButtonWidth, smallButtonHeight, "roomAver", "Buttons/medium");
+            roomLoud = new Button(new Point(screenWidth / 2 +  3 * smallButtonWidth, 380), smallButtonWidth, smallButtonHeight, "roomLoud", "Buttons/high");
+            soundsOn = new Button(new Point(screenWidth / 2 + smallButtonWidth, 485), smallButtonWidth, smallButtonHeight, "soundsOn", "Buttons/on");
+            soundsOff = new Button(new Point(screenWidth / 2 + 3 * smallButtonWidth, 485), smallButtonWidth, smallButtonHeight, "soundsOff", "Buttons/off");
+            setBackground = new Button(new Point(screenWidth / 2 + largeButtonWidth, screenHeight - largeButtonHeight - 40), 
+                largeButtonWidth, largeButtonHeight, "setBackground", "Buttons/setBackground");
+            setGoalImage = new Button(new Point(screenWidth / 2 - 2 * largeButtonWidth, screenHeight - largeButtonHeight - 40), 
+                largeButtonWidth, largeButtonHeight, "setGoalImage", "Buttons/setGoalImage");
 
             buttons = new List<Button>();
             buttons.Add(backButton);
@@ -29,16 +47,24 @@ namespace MazeAndBlue
             buttons.Add(roomQuiet);
             buttons.Add(roomAver);
             buttons.Add(roomLoud);
+            buttons.Add(soundsOn);
+            buttons.Add(soundsOff);
+            buttons.Add(setBackground);
+            buttons.Add(setGoalImage);
         }
 
         public void loadContent()
         {
+            texture = new Texture2D(Program.game.GraphicsDevice, 1, 1);
+            texture = Program.game.Content.Load<Texture2D>("Backgrounds/settingsScreen");
+            
             foreach (Button button in buttons)
                 button.loadContent();
         }
 
         public void draw(SpriteBatch spriteBatch)
         {
+            spriteBatch.Draw(texture, screenRectangle, Color.White);
             foreach (Button button in buttons)
                 button.draw(spriteBatch);
         }
